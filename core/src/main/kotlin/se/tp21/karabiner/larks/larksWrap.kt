9f4f6acae9758757ec5beeefe8b5eced25e601ff
Tom Power <tp21@tuta.io>
2025-07-27 11:08:19 +0100

    tidy

--- core/src/main/kotlin/se/tp21/karabiner/larks/larksWrap.kt
+++ core/src/main/kotlin/se/tp21/karabiner/larks/larksWrap.kt
@@ -2,42 +2,47 @@ package se.tp21.karabiner.larks
 
 import sh.kau.karabiner.*
 import sh.kau.karabiner.ModifierKeyCode.*
+import kotlin.collections.toTypedArray
 
 fun larksWrap(): ComplexModifications =
     ComplexModifications(
         title = "larks wrap",
         rules = listOf(
-            karabinerRule(
+            wrapRule(
                 description = "wrap with \" (left_shift+right_shift+2 to clipboard wrapped with \")",
-                manipulators = listOf(
-                    Manipulator(
-                        from = From(KeyCode.Num2, modifiers = FromModifiers(mandatory = listOf(LeftShift, RightShift))),
-                        to = listOf(
-                            To(keyCode = KeyCode.X, modifiers = listOf(LeftCommand)),
-                            To(shellCommand = "pbpaste | pbcopy -pboard ruler"),
-                            To(keyCode = KeyCode.Quote, modifiers = listOf(LeftShift)),
-                            To(shellCommand = "pbpaste -pboard ruler"),
-                            To(keyCode = KeyCode.V, modifiers = listOf(LeftCommand)),
-                            To(keyCode = KeyCode.Quote, modifiers = listOf(LeftShift))
-                        )
-                    )
-                ).toTypedArray()
+                fromKeyCode = KeyCode.Num2,
+                toOpen = To(keyCode = KeyCode.Quote, modifiers = listOf(LeftShift)),
+                toClose = To(keyCode = KeyCode.Quote, modifiers = listOf(LeftShift)),
+
             ),
-            karabinerRule(
+            wrapRule(
                 description = "wrap with () (left_shift+right_shift+9 to clipboard wrapped with ())",
-                manipulators = listOf(
-                    Manipulator(
-                        from = From(KeyCode.Num9, modifiers = FromModifiers(mandatory = listOf(LeftShift, RightShift))),
-                        to = listOf(
-                            To(keyCode = KeyCode.X, modifiers = listOf(LeftCommand)),
-                            To(shellCommand = "pbpaste | pbcopy -pboard ruler"),
-                            To(keyCode = KeyCode.Num9, modifiers = listOf(LeftShift)),
-                            To(shellCommand = "pbpaste -pboard ruler"),
-                            To(keyCode = KeyCode.V, modifiers = listOf(LeftCommand)),
-                            To(keyCode = KeyCode.Num0, modifiers = listOf(LeftShift))
-                        )
-                    )
-                ).toTypedArray()
-            )
+                fromKeyCode = KeyCode.Num9,
+                toOpen = To(keyCode = KeyCode.Num9, modifiers = listOf(LeftShift)),
+                toClose = To(keyCode = KeyCode.Num0, modifiers = listOf(LeftShift)),
+            ),
         )
+    )
+
+private fun wrapRule(
+    description: String,
+    fromKeyCode: KeyCode,
+    toOpen: To,
+    toClose: To,
+) =
+    karabinerRule(
+        description = description,
+        manipulators = listOf(
+            Manipulator(
+                from = From(fromKeyCode, modifiers = FromModifiers(mandatory = listOf(LeftShift, RightShift))),
+                to = listOf(
+                    To(keyCode = KeyCode.X, modifiers = listOf(LeftCommand)),
+                    To(shellCommand = "pbpaste | pbcopy -pboard ruler"),
+                    toOpen,
+                    To(shellCommand = "pbpaste -pboard ruler"),
+                    To(keyCode = KeyCode.V, modifiers = listOf(LeftCommand)),
+                    toClose
+                )
+            )
+        ).toTypedArray()
     )
