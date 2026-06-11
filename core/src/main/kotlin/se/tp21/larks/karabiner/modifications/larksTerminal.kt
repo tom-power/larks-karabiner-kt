0b77ca09ac3afbf57c1749f13c881d2d5dbef69d
Tom Power <tp21@tuta.io>
2026-06-11 13:36:54 +0100

    add right command -> left control for terminal

--- core/src/main/kotlin/se/tp21/larks/karabiner/modifications/larksTerminal.kt
+++ core/src/main/kotlin/se/tp21/larks/karabiner/modifications/larksTerminal.kt
@@ -3,6 +3,22 @@ package se.tp21.larks.karabiner.modifications
 import sh.kau.karabiner.*
 import sh.kau.karabiner.ModifierKeyCode.*
 
+fun larksTerminalSimple(): List<SimpleModification> =
+    listOf(
+        SimpleModification(
+            from =
+                SimpleModificationKey(
+                    keyCode = RightCommand,
+                ),
+            to =
+                listOf(
+                    SimpleModificationValue(
+                        keyCode = LeftControl,
+                    )
+                )
+        )
+    )
+
 fun larksTerminal(): ComplexModifications =
     ComplexModifications(
         title = "larks terminal",
@@ -10,42 +26,37 @@ fun larksTerminal(): ComplexModifications =
         rules = rules()
     )
 
-private fun rules() =
-    listOf(
-        karabinerRule {
-            description = "Control b (left_command+b to right_control+w)"
-            mapping {
-                fromKey = KeyCode.B
-                fromModifiers = FromModifiers(mandatory = listOf(LeftCommand))
-                toKey = KeyCode.W
-                toModifiers = listOf(RightControl)
-            }
-        },
-        karabinerRule {
-            description = "Control u (left_command+u to right_control+u)"
-            mapping {
-                fromKey = KeyCode.U
-                fromModifiers = FromModifiers(mandatory = listOf(LeftCommand))
-                toKey = KeyCode.U
-                toModifiers = listOf(RightControl)
-            }
-        },
-        karabinerRule {
-            description = "Control l (left_command+l to right_control+l)"
-            mapping {
-                fromKey = KeyCode.L
-                fromModifiers = FromModifiers(mandatory = listOf(LeftCommand))
-                toKey = KeyCode.L
-                toModifiers = listOf(RightControl)
-            }
-        },
-        karabinerRule {
-            description = "Control k (left_command+k to right_control+k)"
-            mapping {
-                fromKey = KeyCode.K
-                fromModifiers = FromModifiers(mandatory = listOf(LeftCommand))
-                toKey = KeyCode.K
-                toModifiers = listOf(RightControl)
-            }
+private fun rules(): List<KarabinerRule> =
+    (keyCodesDifferent() + keyCodesSame())
+        .map { (from, to) ->
+            commandToControlRule(from, to)
         }
+
+private fun commandToControlRule(from: KeyCode, to: KeyCode): KarabinerRule =
+    karabinerRule {
+        description =
+            "Control ${to.name.lowercase()} (left_command+${from.name.lowercase()} to right_control+${to.name.lowercase()})"
+        mapping {
+            fromKey = from
+            fromModifiers = FromModifiers(mandatory = listOf(LeftCommand))
+            toKey = to
+            toModifiers = listOf(RightControl)
+        }
+    }
+
+typealias KeyMapping = Pair<KeyCode, KeyCode>
+
+private fun keyCodesDifferent(): List<KeyMapping> =
+    listOf(
+        KeyCode.B to KeyCode.W
     )
+
+private fun keyCodesSame(): List<KeyMapping> =
+    listOf(
+        KeyCode.U,
+        KeyCode.L,
+        KeyCode.K,
+    )
+        .map { it to it }
+
+
